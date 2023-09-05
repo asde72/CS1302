f853d016fc765ed4e981b9ca51186601d8ad8f5c
asde72 <jjgtech04@gmail.com>
2023-09-05 15:52:09 -0400

    CS1302

---
+++ recursive.py
@@ -0,0 +1,10 @@
+def sumDigits(sum):
+    if sum < 10:
+        return sum
+    else:
+        digit = sum % 10
+        return digit + sumDigits(sum // 10)
+
+Inputer= int(input("Enter an integer: "))
+result = sumDigits(Inputer)
+print(f"The sum of the digits in {Inputer} is {result}.")
